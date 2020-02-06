144e8cb83de6afdaf567ee5b453fdb7efddc0960
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-02-06 18:44:59 -0500

    Get Ignore Practice

---
+++ Git_Ignore_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Ignore_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Ignore Practice.");
+        }
+    }
+}
